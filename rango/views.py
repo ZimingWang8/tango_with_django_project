acb2af93c40e7458b079af89ed93d1456d624767
Ziming_Wang <2737477w@student.gla.ac.uk>
2023-02-18 23:06:42 +0000

    Chapter4

--- rango/views.py
+++ rango/views.py
@@ -8,9 +8,18 @@ from django.http import HttpResponse
 # 每个view至少会用到一个参数——一个HttpRequest对象，它存在于django.http模块
 # 约定将其命名为request，但如果愿意，可以将其重命名为任何名称
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    # 构造一个字典，作为上下文传递给模板引擎
+    # context dictionary是一个将模板变量名boldmessage与Python变量映射在一起的Python字典
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Return a rendered response to send to the client.
+    # 返回一个render的响应发送给客户端，第一个参数是我们希望使用的模板
+    # render()接受用户的请求、模板文件名和context dictionary作为输入。
+    # render()函数将获取这些数据，并将其与模板结合在一起，以生成一个完整的HTML页面，该页面由一个HttpResponse返回。然后这个响应被返回并发送到用户的web浏览器。
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 # 每个view必须返回一个HttpResponse对象。一个简单的HttpResponse对象接受一个字符串参数，该参数表示我们希望发送给请求视图的客户端的页面内容
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Ziming Wang'}
+    return render(request, 'rango/about.html', context=context_dict)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -9,15 +9,27 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(__file__)
+print(os.path.dirname(__file__))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# 这将创建路径Workspace/tango_with_django_project/static/
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+# 这行代码告诉Django，媒体文件将被上传到你的Django项目的<workspace>/tango_with_django_project/media/
+
+MEDIA_ROOT = MEDIA_DIR # 告诉Django在文件系统的这个位置查找已经上传和存储的媒体文件
+MEDIA_URL = '/media/'  # 告诉Django在文件系统从这个URL提供这些文件
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--=mo3l5^f(*jj)vegcw_+xh8sytn)co8k%bj1-y31#ia9fv-il'
@@ -55,7 +67,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', # Check/add this line!
             ],
         },
     },
@@ -117,6 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
 
